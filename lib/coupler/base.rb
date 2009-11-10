1756028deab2dd1c3b462598b5e0036a75dd1012
Jeremy Stephens <viking415@gmail.com>
2009-11-10 11:12:57 -0600

    Started design work

--- lib/coupler/base.rb
+++ lib/coupler/base.rb
@@ -1,5 +1,8 @@
 module Coupler
   class Base < Sinatra::Base
+    set :root, COUPLER_ROOT
+    set :static, true
+    #set :port, 37222
     set :raise_errors, Proc.new { test? }
     set :show_exceptions, Proc.new { development? }
     set :dump_errors, true
@@ -14,6 +17,10 @@ module Coupler
 
     get "/" do
       erb :index
+    end
+
+    get "/test" do
+      erb :lorem_ipsum
     end
   end
 end
