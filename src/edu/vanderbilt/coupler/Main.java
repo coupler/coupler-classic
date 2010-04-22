0b00c3d10159bb292e2df39f56a27e188e065954
Jeremy Stephens <viking415@gmail.com>
2010-04-22 10:01:51 -0500

    Added version information

--- src/edu/vanderbilt/coupler/Main.java
+++ src/edu/vanderbilt/coupler/Main.java
@@ -1,10 +1,39 @@
 package edu.vanderbilt.coupler;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.jruby.embed.ScriptingContainer;
 
 public class Main {
+  private static final Properties properties = new Properties();
+  public static final String PROPFILE = "coupler.properties";
+  static {
+    InputStream stream = null;
+    try {
+      stream = Main.class.getResourceAsStream(PROPFILE);
+      if (stream == null) {
+        throw new RuntimeException("Resource not found: " + PROPFILE);
+      }
+      properties.load(stream);
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    } finally {
+      if (stream != null) {
+        try {
+          stream.close();
+        } catch (IOException e) {
+          // silently ignore
+        }
+      }
+    }
+  }
 
   private Main() {
+    System.out.printf("Coupler version: %s\nBuild date: %s\n\n",
+        properties.getProperty("coupler.version"),
+        properties.getProperty("build.timestamp"));
+
     ScriptingContainer container = new ScriptingContainer();
     String script =
       "require 'coupler/runner'\n" +
