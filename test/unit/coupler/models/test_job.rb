159613fb8afdf94e91b36e6c8283a9de13d4dfcd
Jeremy Stephens <viking415@gmail.com>
2011-04-11 14:29:56 -0500

    Refactored Job tests

--- test/unit/coupler/models/test_job.rb
+++ test/unit/coupler/models/test_job.rb
@@ -1,50 +1,66 @@
-require File.dirname(__FILE__) + '/../../helper'
+require 'helper'
 
 module Coupler
   module Models
     class TestJob < Test::Unit::TestCase
-      def test_sequel_model
+      def new_job(attribs = {})
+        j = Job.new(attribs)
+        if attribs[:resource]
+          j.stubs(:resource_dataset).returns(stub({:all => [attribs[:resource]]}))
+        end
+        if attribs[:scenario]
+          j.stubs(:scenario_dataset).returns(stub({:all => [attribs[:scenario]]}))
+        end
+        j
+      end
+
+      def setup
+        super
+        @resource = stub('resource', :pk => 456, :id => 456, :associations => {})
+        @scenario = stub('scenario', :pk => 456, :id => 456, :associations => {})
+      end
+
+      test "sequel model" do
         assert_equal ::Sequel::Model, Job.superclass
         assert_equal :jobs, Job.table_name
       end
 
-      def test_belongs_to_resource
+      test "belongs to resource" do
         assert_respond_to Job.new, :resource
       end
 
-      def test_belongs_to_scenario
+      test "belongs to scenario" do
         assert_respond_to Job.new, :scenario
       end
 
-      def test_percent_completed
-        job = Factory(:resource_job, :total => 200, :completed => 54)
+      test "percent completed" do
+        job = new_job(:name => 'transform', :resource => @resource, :total => 200, :completed => 54)
         assert_equal 27, job.percent_completed
         job.total = 0
         assert_equal 0, job.percent_completed
       end
 
-      def test_execute_transform
-        job = Factory(:resource_job, :resource => Factory(:resource))
+      test "execute transform" do
+        job = new_job(:name => 'transform', :resource => @resource).save!
 
         now = Time.now
-        Resource.any_instance.expects(:source_dataset_count).returns(12345) # don't like doing this
+        @resource.expects(:source_dataset_count).returns(12345)
         seq = sequence("update")
         job.expects(:update).with(:status => 'running', :total => 12345, :started_at => now).in_sequence(seq)
-        Resource.any_instance.expects(:transform!).in_sequence(seq)
+        @resource.expects(:transform!).in_sequence(seq)
         job.expects(:update).with(:status => 'done', :completed_at => now).in_sequence(seq)
-
         Timecop.freeze(now) { job.execute }
       end
 
-      def test_failed_transform_sets_failed_status
-        job = Factory(:resource_job, :resource => Factory(:resource))
+      test "failed transform sets failed status" do
+        job = new_job(:name => 'transform', :resource => @resource).save!
 
         now = Time.now
-        Resource.any_instance.stubs(:source_dataset_count).returns(12345)
+        @resource.stubs(:source_dataset_count).returns(12345)
         seq = sequence("update")
         job.expects(:update).with(:status => 'running', :total => 12345, :started_at => now).in_sequence(seq)
         fake_exception_klass = Class.new(Exception)
-        Resource.any_instance.expects(:transform!).raises(fake_exception_klass.new).in_sequence(seq)
+        @resource.expects(:transform!).raises(fake_exception_klass.new).in_sequence(seq)
         job.expects(:update).with(:status => 'failed', :completed_at => now).in_sequence(seq)
 
         Timecop.freeze(now) do
@@ -55,26 +71,26 @@ module Coupler
         end
       end
 
-      def test_execute_run_scenario
-        job = Factory(:scenario_job, :scenario => Factory(:scenario))
+      test "execute run scenario" do
+        job = new_job(:name => 'run_scenario', :scenario => @scenario).save!
 
         now = Time.now
         seq = sequence("update")
         job.expects(:update).with(:status => 'running', :started_at => now).in_sequence(seq)
-        Scenario.any_instance.expects(:run!).in_sequence(seq)
+        @scenario.expects(:run!).in_sequence(seq)
         job.expects(:update).with(:status => 'done', :completed_at => now).in_sequence(seq)
 
         Timecop.freeze(now) { job.execute }
       end
 
-      def test_failed_run_scenario_sets_failed_status
-        job = Factory(:scenario_job, :scenario => Factory(:scenario))
+      test "failed run scenario sets failed status" do
+        job = new_job(:name => 'run_scenario', :scenario => @scenario).save!
 
         now = Time.now
         seq = sequence("update")
         job.expects(:update).with(:status => 'running', :started_at => now).in_sequence(seq)
         fake_exception_klass = Class.new(Exception)
-        Scenario.any_instance.expects(:run!).raises(fake_exception_klass.new).in_sequence(seq)
+        @scenario.expects(:run!).raises(fake_exception_klass.new).in_sequence(seq)
         job.expects(:update).with(:status => 'failed', :completed_at => now).in_sequence(seq)
 
         Timecop.freeze(now) do
@@ -85,13 +101,13 @@ module Coupler
         end
       end
 
-      def test_recently_accessed
+      test "recently accessed" do
         now = Time.now
         job_1 = job_2 = job_3 = job_4 = nil
-        Timecop.freeze(now - 3) { job_1 = Factory(:scenario_job, :scenario => Factory(:scenario)) }
-        Timecop.freeze(now - 2) { job_2 = Factory(:scenario_job, :scenario => Factory(:scenario)) }
-        Timecop.freeze(now - 1) { job_3 = Factory(:scenario_job, :scenario => Factory(:scenario)) }
-        Timecop.freeze(now)     { job_4 = Factory(:scenario_job, :scenario => Factory(:scenario)) }
+        Timecop.freeze(now - 3) { job_1 = new_job(:name => "run_scenario", :scenario => @scenario).save! }
+        Timecop.freeze(now - 2) { job_2 = new_job(:name => "run_scenario", :scenario => @scenario).save! }
+        Timecop.freeze(now - 1) { job_3 = new_job(:name => "run_scenario", :scenario => @scenario).save! }
+        Timecop.freeze(now    ) { job_4 = new_job(:name => "run_scenario", :scenario => @scenario).save! }
         assert_equal [job_4, job_3, job_2], Job.recently_accessed
       end
     end
