596d7090fd50ecf55ab4e5697e76aaec42b32ceb
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-09-12 14:42:14 -0500

    Require data and filename fields in Coupler::File

--- lib/coupler/file.rb
+++ lib/coupler/file.rb
@@ -1,4 +1,10 @@
 module Coupler
   class File < Sequel::Model
+    protected
+
+    def validate
+      super
+      validates_presence [:data, :filename]
+    end
   end
 end
--- test/unit/coupler/test_file.rb
+++ test/unit/coupler/test_file.rb
@@ -2,8 +2,25 @@ require 'helper'
 
 module TestCoupler
   class TestFile < Test::Unit::TestCase
+    def new_file(attribs = {})
+      Coupler::File.new({
+        :data => "foo,bar\n123,456",
+        :filename => 'foo.csv'
+      }.merge(attribs))
+    end
+
     test "subclass of Sequel::Model" do
       assert_equal Sequel::Model, Coupler::File.superclass
+    end
+
+    test "requires data" do
+      file = new_file(:data => nil)
+      assert !file.valid?
+    end
+
+    test "requires filename" do
+      file = new_file(:filename => nil)
+      assert !file.valid?
     end
   end
 end
