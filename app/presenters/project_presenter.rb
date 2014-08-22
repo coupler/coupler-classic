9b6f9569fa87be2d9726e12b9648d7f6d7a4f1cb
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-08-22 11:44:14 -0500

    Add ProjectPresenter#forme_input

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -1,5 +1,25 @@
 module Coupler
   class ProjectPresenter
     include Lotus::Presenter
+
+    def forme_input(form, field, opts)
+      opts = {
+        key:   field,
+        value: @object.send(field),
+        label: label_for(field) + ':',
+        labeler: :explicit,
+        wrapper: :div
+      }.merge(opts)
+      type = :text
+
+      Forme::Input.new(form, type, opts)
+    end
+
+    def label_for(field)
+      case field
+      when :name then 'Name'
+      when :description then 'Description'
+      end
+    end
   end
 end
--- spec/presenters/project_presenter_spec.rb
+++ spec/presenters/project_presenter_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
 
 describe ProjectPresenter do
+  let(:project) { double('project', name: 'foo', description: 'bar') }
+  subject(:presenter) { ProjectPresenter.new(project) }
+
+  describe '#forme_input' do
+    let(:form) { double('form') }
+    let(:input) { double('input') }
+
+    describe 'when field is :name' do
+      it 'should create text field' do
+        expect(Forme::Input).to receive(:new).with(form, :text, {
+          key: :name, value: 'foo', label: 'Name:', labeler: :explicit, wrapper: :div
+        }).and_return(input)
+        expect(presenter.forme_input(form, :name, {})).to eql(input)
+      end
+    end
+
+    describe 'when field is :description' do
+      it 'should create text field' do
+        expect(Forme::Input).to receive(:new).with(form, :text, {
+          key: :description, value: 'bar', label: 'Description:', labeler: :explicit, wrapper: :div
+        }).and_return(input)
+        expect(presenter.forme_input(form, :description, {})).to eql(input)
+      end
+    end
+  end
 end
